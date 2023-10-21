e68aa518c90e053dbd92daabe501f2619e4b9d49
iaamlele <LeChen11111@163.com>
2023-10-21 20:19:04 +0800

    add example3.c and example4.c

---
+++ example4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int arr[5000];
+int s = 0;
+
+pthread_mutex_t lock;
+
+void* myfunc(void* args) {
+	pthread_mutex_lock(&lock);
+	for(int i = 0; i < 10000; i++){
+		s += 1;	
+	}
+	pthread_mutex_unlock(&lock);
+	return NULL;
+}
+
+int main(){
+	pthread_t th1;
+	pthread_t th2;
+	pthread_mutex_init(&lock, NULL);
+
+	pthread_create(&th1, NULL, myfunc, NULL);
+	pthread_create(&th2, NULL, myfunc, NULL);
+	pthread_join(th1, NULL);	
+	pthread_join(th2, NULL);	
+	printf("s = %d\n", s);
+	return 0;
+}
